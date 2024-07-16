c81bf0c17d6493fdc6e653f2922a10ec49eba6c4
sun <sun25555@gail.com>
2024-07-16 14:20:38 +0900

    react 파일 생성

---
+++ src/test.js
@@ -0,0 +1,17 @@
+import style from "./css/test1.module.css"
+
+function Test() {
+  return (
+    <div>
+      <ul>
+        <li className="style.sty">가</li>
+        <li>나</li>
+        <li>다</li>
+        <li>다</li>
+        <li>다</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Test;
